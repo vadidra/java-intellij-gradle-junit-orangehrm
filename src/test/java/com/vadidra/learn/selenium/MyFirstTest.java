e90b962183756bc1219b42ac944ddbc84142a97f
vad <vdrayko@mail.com>
2020-09-04 13:06:41 -0700

    Initial commit

---
+++ src/test/java/com/vadidra/learn/selenium/MyFirstTest.java
@@ -0,0 +1,58 @@
+package com.vadidra.learn.selenium;
+
+import static org.junit.Assert.*;
+
+        import org.junit.After;
+        import org.junit.Before;
+        import org.junit.Test;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.support.ui.WebDriverWait;
+
+        import junit.framework.Assert;
+
+        import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @SuppressWarnings("deprecation")
+    @Before
+    public void setUp(){
+
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,20);
+    }
+
+    @After
+    public void tearDown(){
+
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void testLogin(){
+
+        try {
+            driver.get("https://opensource-demo.orangehrmlive.com/");
+            driver.findElement(By.id("txtUsername")).clear();
+            driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+            driver.findElement(By.id("txtPassword")).clear();
+            driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+            driver.findElement(By.id("btnLogin")).click();
+            wait.until(titleIs("OrangeHRM"));
+        }
+        catch (Exception e) {
+            Assert.fail("Exception " + e);
+        }
+
+
+
+    }
+
+}
